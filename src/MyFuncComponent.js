b19b98d01c209ed91e7977c2568aa8b83c2c425d
360BUYAD\yanjiaming1 <yanjiaming1@jd.com>
2021-08-30 14:19:30 +0800

    JSX,组件

---
+++ src/MyFuncComponent.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+export default function MyFuncComp(props) {
+    return <h1>组件内容 {props.number}</h1>
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.Fragment>
+//     <h1>Hello World <span>11</span></h1>
+//     <p>12414</p>
+//   </React.Fragment>,
+//   document.getElementById('root')
+// );
+
+const img = (
+  <img src="https://consumer.gucheng.com/UploadFiles_6578/201806/20180609033740684.jpeg" alt="" />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(img, document.getElementById('root'))
