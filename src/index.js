fe49a086d094c06762c7ede0c0a4d3382617d4a0
360BUYAD\yanjiaming1 <yanjiaming1@jd.com>
2021-09-13 17:44:49 +0800

    2-8

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import TickControl from './components/TickControl'
+import PagerTest from "./components/PagerTest"
 
-// function handelClick(e) {
-//     console.log("点击了");
-// }
 
-// const btn = <button onClick={handelClick} onMouseEnter={() => {
-//     console.log("鼠标移入了")
-// }}>点击我</button>
-
-ReactDOM.render(<TickControl/>, document.getElementById("root"));
+ReactDOM.render( <PagerTest/>, document.getElementById("root"));
