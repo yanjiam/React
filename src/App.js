fab74fbb7b0f78e216cc08418f39a936e8996224
360BUYAD\yanjiaming1 <yanjiaming1@jd.com>
2021-09-23 17:01:06 +0800

    2-14.表单

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import React from 'react'
-import ThreeLayout from './components/common/ThreeLayout'
+import React, { Component } from 'react'
+import FormTest from "./components/FormTest"
 
-export default function App() {
-    return (
-        <div>
-            <ThreeLayout
-                gap={50}
-                leftWidth={200}
-                rightWidth={200}
-                minWidth={800} //最小宽度
-                positionLeft='left' //左侧内容排列规则 left center right
-                positionRight='right' //右侧内容排列规则 left center right
-                positionMain='center' //主区域内容排列规则 left center right
-                left={
-                    <h1>左边</h1>
-                }
-                right={
-                    <h1>右边</h1>
-                }
-            >
-                <h1>主区域</h1>
-            </ThreeLayout>
-        </div>
-    )
+export default class App extends Component {
+
+
+    render() {
+        return <FormTest />
+    }
 }
