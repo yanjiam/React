231cf5a79df95ae8bf22d5f403f300883cecffda
360BUYAD\yanjiaming1 <yanjiaming1@jd.com>
2021-09-28 15:56:13 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import Test from "./components/common/CheckBoxGroup/Test"
-// import Test from "./components/common/RadioBoxGroup/Test"
-// import Test from "./components/common/Select/Test"
-
+import ValidationComp, { A } from "./ValidationComp"
+import Comp from "./Comp"
 export default function App() {
     return (
         <div>
-            <Test/>
+            <ValidationComp
+                a={2}
+                d={<Comp />}
+                e={<Comp />}
+                F={Comp}
+                g={new A()}
+                sex="男"
+                h={[2, 3]}
+                i={{
+                    a: 2
+                }}
+                j={{
+                    a: 3,
+                    name: "abc",
+                    age: 233,
+                    address: {
+                        province: "asdfa",
+                        city: "adsfasdf"
+                    }
+                }}
+                k={[{name:"asdf", age:33}]}
+                m={23}
+                score={"33"}
+            />
         </div>
     )
 }
